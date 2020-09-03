26718df73d25691fd4cb90b78c70ef6560edced6
Nick <inickintosh@me.com>
2020-09-03 15:59:23 -0400

    d

--- scriptbook.js
+++ scriptbook.js
@@ -2,12 +2,12 @@
 
 // Variables to drop HTML:
 var applicationDrop = '<header id="product-banner" class="flex column flex-end align-center"><h2>Mortgage Application</h2></header>';
-
+$( applicationDrop ).insertBefore( $( ".main-content" ) );
 
 $(document).ready(function() {
 
 if (window.location.href.indexOf("loan/apply") != -1) {
-$( applicationDrop ).insertBefore( $( ".main-content" ) );
+
 }
 
 
