1cdff0978a1324a2ebaaa72011ef7cbc5ac1e26b
Nick <inickintosh@me.com>
2020-09-09 01:54:15 -0400

    Update scriptbook.js

--- scriptbook.js
+++ scriptbook.js
@@ -1,5 +1,5 @@
-$(window).load(function(){
-	$('#ssudoux_mortgagebundle_loanapplicationtype_borrower_birth_date').removeClass('.datepicker');
-	$('#sudoux_mortgagebundle_loanapplicationtype_borrower_employment_0_start_date').removeClass('.datepicker');
-	$('#ssudoux_mortgagebundle_loanapplicationtype_borrower_employment_0_end_date').removeClass('.datepicker');
-})
+$(document).ready(function() {
+	$('#sudoux_mortgagebundle_loanapplicationtype_borrower_birth_date').removeClass('datepicker');
+	$('#sudoux_mortgagebundle_loanapplicationtype_borrower_employment_0_start_date').removeClass('datepicker');
+	$('#sudoux_mortgagebundle_loanapplicationtype_borrower_employment_0_end_date').removeClass('datepicker');
+});
