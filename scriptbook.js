12f4231dd5715a7aef74ec08850bcf764601e3cf
Nick <inickintosh@me.com>
2020-09-30 16:04:01 +0000

     

--- scriptbook.js
+++ scriptbook.js
@@ -3,7 +3,7 @@ var bookingLinkLoanOriginatorDrop = '<a href="/page/bookings"><button style="bac
 var prequalLinkLoanOriginatorDrop = '<a href="/loan/graphic-prequal"><button>Prequal</button></a>';
 var borrowerWarning = '<div id="co-borrower-addressing">We are currently having issues accepting co-borrowers with our prequal. This problem is being address. However, we recommend you apply with our full loan application using the Apply link above in the meantime.</div>';
 var covid = '<div id="covid-warning"><h4>To those affected by COVID-19 <span>Find Out More</span></h4><div id="covid-warning-container" class="hide"><p>We are available to review your options with you and explain how to apply for relief. Borrowers interested in contacting AnnieMac to discuss payment assistance during the COVID19 pandemic can reach us using the options below.</p><a href="tel:877-204-1868">Call</a> <a href="mailto:CustomerService@annie-mac.com">Email</a></div></div>';
-var lockedAccount = '<p class="locked-account"><a href="mailto:myaccount@annie-mac.com">Locked Account?</a></p>'
+var lockedAccount = '<p class="locked-account"><a href="mailto:myaccount@annie-mac.com">Locked Account?</a></p>';
 var loanOfficerBoxlet = '<div id="team-site-warning">Please find your loan officer from those listed below and click "More Information" to go to their website to apply.</div>';
 
 $(document).ready(function() {
@@ -12,7 +12,7 @@ $(document).ready(function() {
 	
 // Login Error
 	$(lockedAccount).insertBefore( $( ".user-login .inner-layout #form > p:nth-child(4)" ) );
-	$(".user-login .inner-layout #form  div.alert.alert-error").text("Your email or password is incorrect.  Please check your information and try again.  If you've forgotten your password, please use the 'Forgot Password?' option below.   Accounts will be locked after 3 unsuccessful login attempts.");
+	$(".user-login .inner-layout #form  div.alert.alert-ersror").text("Your email or password is incorrect.  Please check your information and try again.  If you've forgotten your password, please use the 'Forgot Password?' option below.   Accounts will be locked after 3 unsuccessful login attempts.");
 
 // Booking link for loan originators:
 if ($('body').hasClass('site-type-loan_officer')) {
