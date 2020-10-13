9372891df610104220d0d7e7008b94724f8d4b41
Nick <inickintosh@me.com>
2020-10-13 17:54:23 +0000

     

--- scriptbook.js
+++ scriptbook.js
@@ -6,6 +6,10 @@ var lockedAccount = '<p class="locked-account"><a href="mailto:myaccount@annie-m
 var loanOfficerBoxlet = '<div id="team-site-warning">Please find your loan officer from those listed below and click "More Information" to go to their website to apply.</div>';
 
 $(document).ready(function() {
+	if (window.navigator.userAgent.indexOf("Trident/") > 0){
+		if (confirm("Internet Explorer is not supported.\n\n Please update your browser.")){
+		} 
+	}
 	$('#loan-application-nav > li:nth-child(7) > a:nth-child(1) > span:nth-child(1)').text("Final Review");
 	
 	$("#loan-officer-wrapper > label:nth-child(1)").text("If you are working with a loan officer, select from below. If not, please skip.");
