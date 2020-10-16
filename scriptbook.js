67e99b279e8e0d089afc91c5958f480a3b1ebcde
Nick <inickintosh@me.com>
2020-10-16 15:34:49 +0000

     

--- scriptbook.js
+++ scriptbook.js
@@ -213,20 +213,28 @@ $("#covid-warning span").click(function(){
 });
 $(document).ready(function() {
 	$('#sudoux_mortgagebundle_loanapplicationtype_borrower_birth_date').removeClass('datepicker');
+	$('#sudoux_mortgagebundle_loanapplicationtype_borrower_birth_date').addClass('newdatepicker');
 	$("#sudoux_mortgagebundle_loanapplicationtype_borrower_birth_date").prop("placeholder", "MM/DD/YYYY format");
 	$('#sudoux_mortgagebundle_loanapplicationtype_co_borrower_0_birth_date').removeClass('datepicker');
+	$('#sudoux_mortgagebundle_loanapplicationtype_co_borrower_0_birth_date').addClass('newdatepicker');
 	$("#sudoux_mortgagebundle_loanapplicationtype_co_borrower_0_birth_date").prop("placeholder", "MM/DD/YYYY format");
 	$('#sudoux_mortgagebundle_loanapplicationtype_borrower_employment_0_start_date').removeClass('datepicker');
+	$('#sudoux_mortgagebundle_loanapplicationtype_borrower_employment_0_start_date').addClass('newdatepicker');
 	$("#sudoux_mortgagebundle_loanapplicationtype_borrower_employment_0_start_date").prop("placeholder", "MM/DD/YYYY format");
 	$('#sudoux_mortgagebundle_loanapplicationtype_borrower_employment_0_end_date').removeClass('datepicker');
+	$('#sudoux_mortgagebundle_loanapplicationtype_borrower_employment_0_end_date').addClass('newdatepicker');
 	$("#sudoux_mortgagebundle_loanapplicationtype_borrower_employment_0_end_date").prop("placeholder", "MM/DD/YYYY format");
 	$('#sudoux_mortgagebundle_loanapplicationtype_borrower_employment_1_start_date').removeClass('datepicker');
+	$('#sudoux_mortgagebundle_loanapplicationtype_borrower_employment_1_start_date').addClass('newdatepicker');
 	$("#sudoux_mortgagebundle_loanapplicationtype_borrower_employment_1_start_date").prop("placeholder", "MM/DD/YYYY format");
 	$('#sudoux_mortgagebundle_loanapplicationtype_borrower_employment_1_end_date').removeClass('datepicker');
+	$('#sudoux_mortgagebundle_loanapplicationtype_borrower_employment_1_end_date').addClass('newdatepicker');
 	$("#sudoux_mortgagebundle_loanapplicationtype_borrower_employment_1_end_date").prop("placeholder", "MM/DD/YYYY format");
 	$('#sudoux_mortgagebundle_loanapplicationtype_borrower_employment_2_start_date').removeClass('datepicker');
+	$('#sudoux_mortgagebundle_loanapplicationtype_borrower_employment_2_start_date').addClass('newdatepicker');
 	$("#sudoux_mortgagebundle_loanapplicationtype_borrower_employment_2_start_date").prop("placeholder", "MM/DD/YYYY format");
 	$('#sudoux_mortgagebundle_loanapplicationtype_borrower_employment_2_end_date').removeClass('datepicker');
+	$('#sudoux_mortgagebundle_loanapplicationtype_borrower_employment_2_end_date').addClass('newdatepicker');
 	$("#sudoux_mortgagebundle_loanapplicationtype_borrower_employment_2_end_date").prop("placeholder", "MM/DD/YYYY format");
 	$("#sudoux_mortgagebundle_loanapplicationtype_asset_account_0_institution_name").prop("placeholder", "Institution Name");
 	$("#sudoux_mortgagebundle_loanapplicationtype_asset_account_0_account_number").prop("placeholder", "Account Number");
