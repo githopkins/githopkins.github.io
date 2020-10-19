f470e1535a0097c4c72c52515fcb0be67cfacce0
Nick <inickintosh@me.com>
2020-10-19 15:34:50 +0000

     

--- scriptbook.js
+++ scriptbook.js
@@ -227,17 +227,17 @@ $(document).ready(function() {
 	$("#sudoux_mortgagebundle_loanapplicationtype_asset_account_0_institution_name").prop("placeholder", "Institution Name");
 	$("#sudoux_mortgagebundle_loanapplicationtype_asset_account_0_account_number").prop("placeholder", "Account Number");
 	$("#sudoux_mortgagebundle_loanapplicationtype_asset_account_0_balance").prop("placeholder", "Account Balance");
-	var $jqDate = jQuery('.newdatepicker');
-	$jqDate.bind('keyup','keydown', function(e){
-		if(e.which !== 8) {	
-			var numChars = $jqDate.val().length;
-			if(numChars === 2 || numChars === 5){
-				var thisVal = $jqDate.val();
-				thisVal += '-';
-				$jqDate.val(thisVal);
-			}
-	  }
-	});
+	// var $jqDate = jQuery('.newdatepicker');
+	// $jqDate.bind('keyup','keydown', function(e){
+	// 	if(e.which !== 8) {	
+	// 		var numChars = $jqDate.val().length;
+	// 		if(numChars === 2 || numChars === 5){
+	// 			var thisVal = $jqDate.val();
+	// 			thisVal += '-';
+	// 			$jqDate.val(thisVal);
+	// 		}
+	//   }
+	// });
 	$( "button" ).click(function() {
 		$('#sudoux_mortgagebundle_loanapplicationtype_borrower_employment_1_start_date').removeClass('datepicker');
 		$('#sudoux_mortgagebundle_loanapplicationtype_borrower_employment_1_start_date').attr("type", "date");
@@ -269,17 +269,17 @@ $(document).ready(function() {
 		$('#sudoux_mortgagebundle_loanapplicationtype_borrower_employment_5_end_date').removeClass('datepicker');
 		$('#sudoux_mortgagebundle_loanapplicationtype_borrower_employment_5_end_date').attr("type", "date");
 		$("#sudoux_mortgagebundle_loanapplicationtype_borrower_employment_5_end_date").prop("placeholder", "MM-DD-YYYY format");
-		var $jqDates = jQuery('.newdatepickertwo');
-		$jqDates.bind('keyup','keydown', function(e){
-			if(e.which !== 8) {	
-				var numChars = $jqDates.val().length;
-				if(numChars === 2 || numChars === 5){
-					var thisVal = $jqDates.val();
-					thisVal += '-';
-					$jqDates.val(thisVal);
-				}
-		  }
-		});
+		// var $jqDates = jQuery('.newdatepickertwo');
+		// $jqDates.bind('keyup','keydown', function(e){
+		// 	if(e.which !== 8) {	
+		// 		var numChars = $jqDates.val().length;
+		// 		if(numChars === 2 || numChars === 5){
+		// 			var thisVal = $jqDates.val();
+		// 			thisVal += '-';
+		// 			$jqDates.val(thisVal);
+		// 		}
+		//   }
+		// });
 	});
 	
 	$("#add-asset-account").click(function(){
