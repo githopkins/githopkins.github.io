25a7e6232e94e3c6d6bb0d4b4f58fb59f2858221
Nick <inickintosh@me.com>
2020-10-21 18:47:51 +0000

     

--- accordion.js
+++ accordion.js
@@ -1,12 +1,17 @@
 $(document).ready(function() {
-	$('link[href="/dev/themes/annie_mac/styles.css"]').prop('disabled', true);
-	$('link[href="/dev/themes/annie_mac/styles.css"]').attr('disabled', 'disabled');
-	$('.accordion-selection').click(function() {
-		$('.toggle', this).toggle();
-		$('.hide', this).toggle();
-	});
-	$('#expand-all').click(function() {
-		$('.toggle').toggle();
-		$('.accordion-selection .hide').toggle();
-	});
+
+// Disable conflicting stylesheets.
+$('link[href="/dev/themes/annie_mac/styles.css"]').prop('disabled', true);
+$('link[href="/dev/themes/annie_mac/styles.css"]').attr('disabled', 'disabled');
+
+$('.accordion-selection').click(function() {
+	$('.toggle', this).toggle();
+	$('.hide', this).toggle();
+});
+
+$('#expand-all').click(function() {
+	$('.toggle').toggle();
+	$('.accordion-selection .hide').toggle();
+});
+	
 });
