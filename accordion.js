9bf0f16386372f0e6f38e632640120505a4807cf
Nick <inickintosh@me.com>
2020-10-21 18:22:55 +0000

     

--- accordion.js
+++ accordion.js
@@ -1,9 +1,8 @@
 $(document).ready(function() {
-		
-$('.accordion-selection').click(function() {
-	$('.toggle', this).toggle();
-	$('.hide', this).toggle();
-});
-
-
+	$('link[href="/dev/themes/annie_mac/styles.css"]').prop('disabled', true);
+	$('link[href="/dev/themes/annie_mac/styles.css"]').attr('disabled', 'disabled');
+	$('.accordion-selection').click(function() {
+		$('.toggle', this).toggle();
+		$('.hide', this).toggle();
+	});
 });
