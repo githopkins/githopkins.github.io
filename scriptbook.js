940c1bf42627ee8c81f38c82847504d6cf82b52f
Nick <inickintosh@me.com>
2020-11-02 20:54:44 +0000

     

--- scriptbook.js
+++ scriptbook.js
@@ -2,7 +2,7 @@
 var bookingLinkLoanOriginatorDrop = '<a href="/page/bookings"><button style="background: #78c254">Book</button></a>';
 var prequalLinkLoanOriginatorDrop = '<a href="/loan/graphic-prequal"><button>Prequal</button></a>';
 var covid = '<div id="covid-warning"><h4>To those affected by COVID-19 <span>Find Out More</span></h4><div id="covid-warning-container" class="hide"><p>We are available to review your options with you and explain how to apply for relief. Borrowers interested in contacting AnnieMac to discuss payment assistance during the COVID19 pandemic can reach us using the options below.</p><a href="tel:877-204-1868">Call</a> <a href="mailto:CustomerService@annie-mac.com">Email</a></div></div>';
-var lockedAccount = '<p class="locked-account"><a href="mailto:myaccount@annie-mac.com" class="link">Locked Account?</a><br/><a href="www.anniemacservicing.com" class="link">Looking to make a payment? Click here.</a></p>';
+var lockedAccount = '<p class="locked-account"><a href="mailto:myaccount@annie-mac.com" class="link">Locked Account?</a><br/><a href="www.anniemacservicing.com">Looking to make a payment? <span class="link">Click here.</span></a></p>';
 var loanOfficerBoxlet = '<div id="team-site-warning">Please find your loan officer from those listed below and click "More Information" to go to their website to apply.</div>';
 
 $(document).ready(function() {
