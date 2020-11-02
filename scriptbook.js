f1e08a58771f4c3ad06dcbb59df9e20b67cd1a3f
Nick <inickintosh@me.com>
2020-11-02 20:51:28 +0000

     

--- scriptbook.js
+++ scriptbook.js
@@ -13,7 +13,6 @@ $(document).ready(function() {
 	
 // Login Error
 	$(lockedAccount).insertBefore( $( ".user-login .inner-layout #form > p:nth-child(4)" ) );
-	$(alreadyBorrower).insertBefore( $( ".user-login .inner-layout #form > p:nth-child(4)" ) );
 	$(".user-login .inner-layout #form  div.alert.alert-error").text("Your email or password is incorrect.  Please check your information and try again.  If you've forgotten your password, please use the 'Forgot Password?' option below.   Accounts will be locked after 3 unsuccessful login attempts.");
 
 // Booking link for loan originators:
