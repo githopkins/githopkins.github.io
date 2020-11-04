fceb273079b98696eb0ba280300f88840910dd25
Nick <inickintosh@me.com>
2020-11-04 21:30:32 +0000

     

--- scriptbook.js
+++ scriptbook.js
@@ -4,7 +4,7 @@ var prequalLinkLoanOriginatorDrop = '<a href="/loan/graphic-prequal"><button>Pre
 var covid = '<div id="covid-warning"><h4>To those affected by COVID-19 <span>Find Out More</span></h4><div id="covid-warning-container" class="hide"><p>We are available to review your options with you and explain how to apply for relief. Borrowers interested in contacting AnnieMac to discuss payment assistance during the COVID19 pandemic can reach us using the options below.</p><a href="tel:877-204-1868">Call</a> <a href="mailto:CustomerService@annie-mac.com">Email</a></div></div>';
 var lockedAccount = '<p class="locked-account"><a href="mailto:myaccount@annie-mac.com" class="link">Is your account locked?</a><br/><a href="https://www.anniemacservicing.com" class="link">Looking to manage a payment?</a></p>';
 var loanOfficerBoxlet = '<div id="team-site-warning">Please find your loan officer from those listed below and click "More Information" to go to their website to apply.</div>';
-var myservice = '<button><a href="https://www.anniemacservicing.com">Manage Mortgage</a></button>';
+var myservice = '<button><a href="https://www.anniemacservicing.com">Pay My Mortgage</a></button>';
 
 $(document).ready(function() {
 	$("body > nav > div.top-links-header.order-1 > div > a:nth-child(2) > button").text("My Application");
