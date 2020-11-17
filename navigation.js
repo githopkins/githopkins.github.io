6ad52706ff89f2a72656652cf6d2f37bd264c077
Nick Hopkins <nick@nicks-macbook-pro.local>
2020-11-17 19:35:06 +0000

     

--- navigation.js
+++ navigation.js
@@ -2,4 +2,8 @@ $( document ).ready(function() {
 	$( "#side-bar-navigation-toggle" ).click(function() {
 		$('#navigation-sidebar').toggleClass('hide');
 	});
+	
+	$( "#close-navigation-side" ).click(function() {
+		$('#navigation-sidebar').toggleClass('hide');
+	});
 });
