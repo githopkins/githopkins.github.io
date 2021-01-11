5bef532c1aca3ea5abf261e0db32920c122bfedf
Nick Hopkins <nick@macbook-pro.local>
2021-01-11 16:13:11 +0000

     

--- scriptbook.js
+++ scriptbook.js
@@ -171,6 +171,12 @@ $("#covid-warning span").click(function(){
 		$("#team-display > div.team-member-section > div:nth-child(1)").prependTo(".branch-manager-section");
 		$("#team-display > h3:nth-child(3)").hide();
 	}
+	if (
+		(window.location.href.indexOf("louisville.annie-mac.com") != -1) || 
+		(window.location.href.indexOf("mylendingrate.com") != -1)
+		){
+		$("#team-display > div.team-member-section").prepend("<div id='' class='team-member'><div id='' class='team-member-information'><h4>Chrissy Pierson</h4><h5>Mortgage Loan Originator</h5><h5>NMLS: 1412881</h5><div class='team-member-portrait'><img src='http://annie-mac.com/uploads/sites/10713/public/ChristinaPierson_Selects_0123.jpeg' style='border-radius: 4px'></div><button class='go flex space-between align-center'><a href='http://chrissypierson.annie-mac.com' class='btn button-main'>More Information</a><i class='fas fa-globe-americas'></i></button><button class='flex space-between align-center'>(513) 769-2038<i class='fas fa-mobile-alt'></i></button><button class='flex space-between align-center'><a href='mailto: cpierson@annie-mac.com'>Email Me</a><i class='fas fa-envelope'></i></button></div></div>");
+	}
 	if (window.location.href.indexOf("louisville.annie-mac.com") != -1) {
 		$("#team-display > div.team-member-section > div:nth-child(5)").addClass("manager-information");
 		$("#team-display > div.team-member-section > div:nth-child(5)").removeClass("team-member");
@@ -178,12 +184,6 @@ $("#covid-warning span").click(function(){
 		$("#team-display > div.team-member-section > div:nth-child(2)").addClass("manager-information");
 		$("#team-display > div.team-member-section > div:nth-child(2)").removeClass("team-member");
 		$("#team-display > div.team-member-section > div:nth-child(2)").prependTo(".branch-manager-section");
-	}
-	if (
-		(window.location.href.indexOf("louisville.annie-mac.com") != -1) || 
-		(window.location.href.indexOf("mylendingrate.com") != -1)
-		){
-		$("#team-display > div.team-member-section").prepend("<div id='' class='team-member'><div id='' class='team-member-information'><h4>Chrissy Pierson</h4><h5>Mortgage Loan Originator</h5><h5>NMLS: 1412881</h5><div class='team-member-portrait'><img src='http://annie-mac.com/uploads/sites/10713/public/ChristinaPierson_Selects_0123.jpeg' style='border-radius: 4px'></div><button class='go flex space-between align-center'><a href='http://chrissypierson.annie-mac.com' class='btn button-main'>More Information</a><i class='fas fa-globe-americas'></i></button><button class='flex space-between align-center'>(513) 769-2038<i class='fas fa-mobile-alt'></i></button><button class='flex space-between align-center'><a href='mailto: cpierson@annie-mac.com'>Email Me</a><i class='fas fa-envelope'></i></button></div></div>");
 	}
 	// Team: Andy Sikora
 	if (window.location.href.indexOf("reston.annie-mac.com") != -1) {
