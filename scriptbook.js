11e7d24cc866bd9645a848ba7113996c11a42ed9
Nick Hopkins <nick@macbook-pro.local>
2021-02-10 22:10:22 +0000

     

--- scriptbook.js
+++ scriptbook.js
@@ -21,34 +21,34 @@ $( document ).ready(function() {
 		$("#biography > div.manager-biography > h2").text('About ' + firstname[0]);
 	};
 	// Domains
-	if (
-		(window.location.href.indexOf("jamiehannigan.annie-mac.com") > -1) || 		
-		(window.location.href.indexOf("joeharrison.annie-mac.com") > -1) || 
-		(window.location.href.indexOf("tomfox.annie-mac.com") > -1) || 
-		(window.location.href.indexOf("shawnyocum.annie-mac.com") > -1) || 
-		(window.location.href.indexOf("raymondmoore.annie-mac.com") > -1) || 
-		(window.location.href.indexOf("randyfiocchi.annie-mac.com") > -1) || 
-		(window.location.href.indexOf("jamiehannigan.annie-mac.com") > -1) || 
-		(window.location.href.indexOf("michaelspillman.annie-mac.com") > -1) || 
-		(window.location.href.indexOf("matthewsiderio.annie-mac.com") > -1) || 
-		(window.location.href.indexOf("jesseschwager.annie-mac.com") > -1) || 
-		(window.location.href.indexOf("jasontrotte.annie-mac.com") > -1) || 
-		(window.location.href.indexOf("jasonleibowitz.annie-mac.com") > -1) || 
-		(window.location.href.indexOf("edyucis.annie-mac.com") > -1) || 
-		(window.location.href.indexOf("dominiccarlucci.annie-mac.com") > -1) || 
-		(window.location.href.indexOf("dajshiagibson.annie-mac.com") > -1) || 
-		(window.location.href.indexOf("christamartin.annie-mac.com") > -1) || 
-		(window.location.href.indexOf("cathymiller.annie-mac.com") > -1) || 
-		(window.location.href.indexOf("brianvoytko.annie-mac.com") > -1) || 
-		(window.location.href.indexOf("amysodowich.annie-mac.com") > -1) || 
-		(window.location.href.indexOf("aarontedrow.annie-mac.com") > -1)
-		) {
-		$("#navigation-logo > a > img").attr("src", "https://annie-mac.com/uploads/sites/11832/public/7283-SEPT2020_AM-Community-Mortgage-Team-Logo-WEBSITE.png");
-		$("#navigation-logo > a > img").css("margin-top", "6px");
-	};
+	// if (
+	// 	(window.location.href.indexOf("jamiehannigan.annie-mac.com") > -1) || 		
+	// 	(window.location.href.indexOf("joeharrison.annie-mac.com") > -1) || 
+	// 	(window.location.href.indexOf("tomfox.annie-mac.com") > -1) || 
+	// 	(window.location.href.indexOf("shawnyocum.annie-mac.com") > -1) || 
+	// 	(window.location.href.indexOf("raymondmoore.annie-mac.com") > -1) || 
+	// 	(window.location.href.indexOf("randyfiocchi.annie-mac.com") > -1) || 
+	// 	(window.location.href.indexOf("jamiehannigan.annie-mac.com") > -1) || 
+	// 	(window.location.href.indexOf("michaelspillman.annie-mac.com") > -1) || 
+	// 	(window.location.href.indexOf("matthewsiderio.annie-mac.com") > -1) || 
+	// 	(window.location.href.indexOf("jesseschwager.annie-mac.com") > -1) || 
+	// 	(window.location.href.indexOf("jasontrotte.annie-mac.com") > -1) || 
+	// 	(window.location.href.indexOf("jasonleibowitz.annie-mac.com") > -1) || 
+	// 	(window.location.href.indexOf("edyucis.annie-mac.com") > -1) || 
+	// 	(window.location.href.indexOf("dominiccarlucci.annie-mac.com") > -1) || 
+	// 	(window.location.href.indexOf("dajshiagibson.annie-mac.com") > -1) || 
+	// 	(window.location.href.indexOf("christamartin.annie-mac.com") > -1) || 
+	// 	(window.location.href.indexOf("cathymiller.annie-mac.com") > -1) || 
+	// 	(window.location.href.indexOf("brianvoytko.annie-mac.com") > -1) || 
+	// 	(window.location.href.indexOf("amysodowich.annie-mac.com") > -1) || 
+	// 	(window.location.href.indexOf("aarontedrow.annie-mac.com") > -1)
+	// 	) {
+	// 	$("#navigation-logo > a > img").attr("src", "https://annie-mac.com/uploads/sites/11832/public/7283-SEPT2020_AM-Community-Mortgage-Team-Logo-WEBSITE.png");
+	// 	$("#navigation-logo > a > img").css("margin-top", "6px");
+	// };
 	if (window.location.href.indexOf("themtgco.com") > -1) {	
-		$("body > nav > div.top-links-header.order-1 > div > a.tel").text("855-559-8651");
-		$("body > nav > div.top-links-header.order-1 > div > a.tel").attr("href", "tel:855-559-8651");
+		// $("body > nav > div.top-links-header.order-1 > div > a.tel").text("855-559-8651");
+		// $("body > nav > div.top-links-header.order-1 > div > a.tel").attr("href", "tel:855-559-8651");
 	}
 	// Team: Autullo
 	if (window.location.href.indexOf("cincinnati.annie-mac.com") > -1) {
@@ -129,79 +129,4 @@ $( document ).ready(function() {
 		$("#navigation-logo > a > img").attr("src", "https://annie-mac.com/uploads/sites/10713/public/hulsey.png");
 		$("#navigation-logo > a > img").css("margin-top", "3px");
 	}
-	// Team: Barrow		
-	if (window.location.href.indexOf("teambarrow.annie-mac.com") > -1) {	
-		 $("#branch-content > h2").text("Team Barrow");		
-		 $(document).prop('title', 'Team Barrow');		
-		 $("#branch-content > h5 > a.tel").text("(252) 917-8400");
-		 $("#branch-content > h5 > a.tel").attr("href", "tel:252-917-8400");
-		  var people = $(".team-member");
-		  people.each((index, teamMember) => {
-			  if (![1].includes(index)) {
-				  teamMember.style.display = "none"
-			  }
-		  })	
-		  // Hide options from dropdown on team page that don't include only specified loan originators.		
-		 $("#sudoux_mortgagebundle_loanapplicationtype_loan_officer > option").each((i, option) => {		
-			 value = option.getAttribute('value');		
-			 // The following are the ID values for the loan officers who are able to be seleted from the drop down. Everyone whose ID does not match is hidden.		
-			 if (value !== "") {		
-				 option.setAttribute('hidden', 'true');		
-			 }		
-		 })
-		 $('#sudoux_mortgagebundle_loanapplicationtype_loan_officer').find('option[value=9069]').attr('selected','selected');
-		 $('#loan-officer-wrapper').hide();
-	 }
-	// Team: Fillyaw
-	if (window.location.href.indexOf("teamfillyaw.annie-mac.com") > -1) {
-		$("#navigation-logo > a > img").attr("src", "https://annie-mac.com/uploads/sites/10713/public/TeamFillyaw.png");
-		$("#navigation-logo > a > img").css("margin-top", "0px");
-		$("#branch-content > h2").text("Team Fillyaw");
-		$(document).prop('title', 'Team Fillyaw');
-		// Hide loan originators who aren't Troy or Michael based on index of all team members.
-		var people = $(".team-member");
-		people.each((index, teamMember) => {
-			if (![0, 2].includes(index)) {
-				teamMember.style.display = "none"
-			}
-		})
-		// Hide options from dropdown on team page that don't include only specified loan originators.
-		$("#sudoux_mortgagebundle_loanapplicationtype_loan_officer > option").each((i, option) => {
-			value = option.getAttribute('value');
-			// The following are the ID values for the loan officers who are able to be seleted from the drop down. Everyone whose ID does not match is hidden.
-			if (value !== "9246" && value !== "9272") {
-				option.setAttribute('hidden', 'true');
-			}
-		})
-	}
-	// Team: Bruner - Includes New Smyrna and Winter Park
-	if (window.location.href.indexOf("teambruner.annie-mac.com") > -1) {
-		$("#branch-content > h2").text("Team Bruner");
-		$(document).prop('title', 'Team Bruner');		
-	};
-	if (window.location.href.indexOf("newsmyrna.annie-mac.com") > -1) {
-			$("body > div.team-member-section > div:nth-child(4)").addClass("manager-information");
-			$("body > div.team-member-section > div:nth-child(4)").removeClass("team-member");
-			$("body > div.team-member-section > div:nth-child(4)").prependTo("#branch-managers");
-			$("body > div.team-member-section > div:nth-child(1) > div > button:nth-child(6)").html("(386) 427-3445 <i class='fas fa-mobile-alt'></i>");
-			$("body > div.team-member-section > div:nth-child(3) > div > button:nth-child(6)").html("(407) 205-0190 x1816 <i class='fas fa-mobile-alt'></i>");
-			$("#branch-managers > div > div > button:nth-child(6)").html("386-427-3445 <i class='fas fa-mobile-alt'></i>");
-	};
-	if (
-		(window.location.href.indexOf("teambruner.annie-mac.com") > -1) || 
-		(window.location.href.indexOf("winterpark.annie-mac.com") > -1)
-		){
-		$("#team-display > div.team-member-section > div:nth-child(2)").addClass("manager-information");
-		$("#team-display > div.team-member-section > div:nth-child(2)").removeClass("team-member");
-		$("#team-display > div.team-member-section > div:nth-child(2)").prependTo(".branch-manager-section");
-		$("#team-display > div.team-member-section > div > div > button:nth-child(6)").html("(407) 205-0190 x1816 <i class='fas fa-mobile-alt'></i>");
-		$("#branch-managers > div > div:nth-child(1) > div > button:nth-child(6)").html("(386) 427-3445 <i class='fas fa-mobile-alt'></i>");
-	};
-	if (window.location.href.indexOf("chesapeake.annie-mac.com") > -1) { 
-		$("#team-display > div.team-member-section > div:nth-child(1)").addClass("manager-information");
-		$("#team-display > div.team-member-section > div:nth-child(1)").removeClass("team-member");
-		$("#team-display > div.team-member-section > div:nth-child(1)").prependTo(".branch-manager-section");
-		$("#team-display > h3:nth-child(3)").hide();
-	}
-}
-);
+});
