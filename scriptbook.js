dce02aedef6a4327759573dcd7d957fe3136bdf1
Nick Hopkins <nick@macbook-pro.local>
2021-02-10 22:55:37 +0000

     

--- scriptbook.js
+++ scriptbook.js
@@ -17,7 +17,8 @@ $( document ).ready(function() {
 		$(fullwidthsidebarlinks).appendTo( $( "#loan-originator-backdrop" ) );
 		$("#we-provide-more-master > h3").text(firstname[0] + ' Provides...');
 		$("#biography > div.manager-biography > h2").text('About ' + firstname[0]);
-	});
+	};
+});
 	// Domains
 	// if (
 	// 	(window.location.href.indexOf("jamiehannigan.annie-mac.com") > -1) || 		
@@ -44,88 +45,86 @@ $( document ).ready(function() {
 	// 	$("#navigation-logo > a > img").attr("src", "https://annie-mac.com/uploads/sites/11832/public/7283-SEPT2020_AM-Community-Mortgage-Team-Logo-WEBSITE.png");
 	// 	$("#navigation-logo > a > img").css("margin-top", "6px");
 	// };
-	if (window.location.href.indexOf("themtgco.com") > -1) {	
-		// $("body > nav > div.top-links-header.order-1 > div > a.tel").text("855-559-8651");
-		// $("body > nav > div.top-links-header.order-1 > div > a.tel").attr("href", "tel:855-559-8651");
-	}
-	// Team: Autullo
-	if (window.location.href.indexOf("cincinnati.annie-mac.com") > -1) {
-		$("#navigation-logo > a > img").attr("src", "https://annie-mac.com/uploads/sites/10713/public/Autullo.png");
-		$("#navigation-logo > a > img").css("margin-top", "0px");
-	}
-	// Team: Cartmel Group
-	if (window.location.href.indexOf("indianapolis.annie-mac.com") > -1) {
-		$("#navigation-logo > a > img").attr("src", "https://annie-mac.com/uploads/sites/10713/public/Cartmel.png");
-		$("#navigation-logo > a > img").css("margin-top", "0px");
-	}
-	// Team: Craig Sanders
-	if (window.location.href.indexOf("hendersonville.annie-mac.com") > -1) {
-		$("#navigation-logo > a > img").attr("src", "https://annie-mac.com/uploads/sites/10713/public/CraigSanders.png");
-		$("#navigation-logo > a > img").css("margin-top", "5px");
-	}
-	// Team: Northwest Lending
-	if (window.location.href.indexOf("lakeoswego.annie-mac.com") > -1) {
-		$("#navigation-logo > a > img").css("margin-top", "5px");
-	}
-	// Team: East Brunswick
-	if (window.location.href.indexOf("eastbrunswick.annie-mac.com") > -1) {
-		$("#navigation-logo > a > img").attr("src", "https://annie-mac.com/uploads/sites/10713/public/EastBrunswick.png");
-		$("#navigation-logo > a > img").css("margin-top", "6px");
-	}
-	// Team: Hurd Home
-	if (window.location.href.indexOf("newportnews.annie-mac.com") > -1) {
-		$("#navigation-logo > a > img").attr("src", "https://annie-mac.com/uploads/sites/10713/public/Hurd.png");
-		$("#navigation-logo > a > img").css("margin-top", "4px");
-	}
-	// Team: Jesse Schwager
-	if (window.location.href.indexOf("jesseschwager.annie-mac.com") > -1) {
-		$("#navigation-logo > a > img").attr("src", "https://annie-mac.com/uploads/sites/10713/public/JesseSchwager.png");
-		$("#navigation-logo > a > img").css("margin-top", "0px");
-	}
-	// Team: Jimmy Valley
-	if (window.location.href.indexOf("jimmyvalley.annie-mac.com") > -1) {
-		$("#navigation-logo > a > img").attr("src", "https://annie-mac.com/uploads/sites/10713/public/JimmyValley.png");
-		$("#navigation-logo > a > img").css("margin-top", "4px");
-	}
-	// Team: Michelle Miller
-	if (window.location.href.indexOf("warsaw.annie-mac.com") > -1) {
-		$("#navigation-logo > a > img").attr("src", "https://annie-mac.com/uploads/sites/10713/public/MichelleMiller.png");
-		$("#navigation-logo > a > img").css("margin-top", "4px");
-	}
-	// Team: The Mills Group
-	if (window.location.href.indexOf("crofton.annie-mac.com") > -1) {
-		$("#navigation-logo > a > img").attr("src", "https://annie-mac.com/uploads/sites/10713/public/MillsGroup.png");
-		$("#navigation-logo > a > img").css("margin-top", "7px");
-	}
-	// Team: Mortgages Diversified
-	if (window.location.href.indexOf("edenprairie.annie-mac.com") > -1) {
-		$("#navigation-logo > a > img").attr("src", "https://annie-mac.com/uploads/sites/10713/public/MortgagesDiversified.png");
-		$("#navigation-logo > a > img").css("margin-top", "7px");
-	}
-	// Team: The Purchase Pros
-	if (window.location.href.indexOf("wall.annie-mac.com") > -1) {
-		$("#navigation-logo > a > img").attr("src", "https://annie-mac.com/uploads/sites/10713/public/PurchasePros.png");
-		$("#navigation-logo > a > img").css("margin-top", "0px");
-	}
-	// Team: Team Shealey
-	if (window.location.href.indexOf("katy.annie-mac.com") > -1) {
-		$("#navigation-logo > a > img").attr("src", "https://annie-mac.com/uploads/sites/10713/public/Shealey.png");
-		$("#navigation-logo > a > img").css("margin-top", "0px");
-	}
-	// Team: The Home Team
-	if (window.location.href.indexOf("stevevetter.annie-mac.com") > -1) {
-		$("#navigation-logo > a > img").attr("src", "https://annie-mac.com/uploads/sites/10713/public/TheHomeTeam.png");
-		$("#navigation-logo > a > img").css("margin-top", "7px");
-	}
-	// Team: Trye
-	if (window.location.href.indexOf("towson.annie-mac.com") > -1) {
-		$("#navigation-logo > a > img").attr("src", "https://annie-mac.com/uploads/sites/10713/public/Trye.png");
-		$("#navigation-logo > a > img").css("margin-top", "0px");
-	}
-	// Team: kevinhulsey
-	if (window.location.href.indexOf("kevinhulsey.annie-mac.com") > -1) {
-		$("#navigation-logo > a > img").attr("src", "https://annie-mac.com/uploads/sites/10713/public/hulsey.png");
-		$("#navigation-logo > a > img").css("margin-top", "3px");
-	}
-});
-);
+	// if (window.location.href.indexOf("themtgco.com") > -1) {	
+	// 	// $("body > nav > div.top-links-header.order-1 > div > a.tel").text("855-559-8651");
+	// 	// $("body > nav > div.top-links-header.order-1 > div > a.tel").attr("href", "tel:855-559-8651");
+	// }
+	// // Team: Autullo
+	// if (window.location.href.indexOf("cincinnati.annie-mac.com") > -1) {
+	// 	$("#navigation-logo > a > img").attr("src", "https://annie-mac.com/uploads/sites/10713/public/Autullo.png");
+	// 	$("#navigation-logo > a > img").css("margin-top", "0px");
+	// }
+	// // Team: Cartmel Group
+	// if (window.location.href.indexOf("indianapolis.annie-mac.com") > -1) {
+	// 	$("#navigation-logo > a > img").attr("src", "https://annie-mac.com/uploads/sites/10713/public/Cartmel.png");
+	// 	$("#navigation-logo > a > img").css("margin-top", "0px");
+	// }
+	// // Team: Craig Sanders
+	// if (window.location.href.indexOf("hendersonville.annie-mac.com") > -1) {
+	// 	$("#navigation-logo > a > img").attr("src", "https://annie-mac.com/uploads/sites/10713/public/CraigSanders.png");
+	// 	$("#navigation-logo > a > img").css("margin-top", "5px");
+	// }
+	// // Team: Northwest Lending
+	// if (window.location.href.indexOf("lakeoswego.annie-mac.com") > -1) {
+	// 	$("#navigation-logo > a > img").css("margin-top", "5px");
+	// }
+	// // Team: East Brunswick
+	// if (window.location.href.indexOf("eastbrunswick.annie-mac.com") > -1) {
+	// 	$("#navigation-logo > a > img").attr("src", "https://annie-mac.com/uploads/sites/10713/public/EastBrunswick.png");
+	// 	$("#navigation-logo > a > img").css("margin-top", "6px");
+	// }
+	// // Team: Hurd Home
+	// if (window.location.href.indexOf("newportnews.annie-mac.com") > -1) {
+	// 	$("#navigation-logo > a > img").attr("src", "https://annie-mac.com/uploads/sites/10713/public/Hurd.png");
+	// 	$("#navigation-logo > a > img").css("margin-top", "4px");
+	// }
+	// // Team: Jesse Schwager
+	// if (window.location.href.indexOf("jesseschwager.annie-mac.com") > -1) {
+	// 	$("#navigation-logo > a > img").attr("src", "https://annie-mac.com/uploads/sites/10713/public/JesseSchwager.png");
+	// 	$("#navigation-logo > a > img").css("margin-top", "0px");
+	// }
+	// // Team: Jimmy Valley
+	// if (window.location.href.indexOf("jimmyvalley.annie-mac.com") > -1) {
+	// 	$("#navigation-logo > a > img").attr("src", "https://annie-mac.com/uploads/sites/10713/public/JimmyValley.png");
+	// 	$("#navigation-logo > a > img").css("margin-top", "4px");
+	// }
+	// // Team: Michelle Miller
+	// if (window.location.href.indexOf("warsaw.annie-mac.com") > -1) {
+	// 	$("#navigation-logo > a > img").attr("src", "https://annie-mac.com/uploads/sites/10713/public/MichelleMiller.png");
+	// 	$("#navigation-logo > a > img").css("margin-top", "4px");
+	// }
+	// // Team: The Mills Group
+	// if (window.location.href.indexOf("crofton.annie-mac.com") > -1) {
+	// 	$("#navigation-logo > a > img").attr("src", "https://annie-mac.com/uploads/sites/10713/public/MillsGroup.png");
+	// 	$("#navigation-logo > a > img").css("margin-top", "7px");
+	// }
+	// // Team: Mortgages Diversified
+	// if (window.location.href.indexOf("edenprairie.annie-mac.com") > -1) {
+	// 	$("#navigation-logo > a > img").attr("src", "https://annie-mac.com/uploads/sites/10713/public/MortgagesDiversified.png");
+	// 	$("#navigation-logo > a > img").css("margin-top", "7px");
+	// }
+	// // Team: The Purchase Pros
+	// if (window.location.href.indexOf("wall.annie-mac.com") > -1) {
+	// 	$("#navigation-logo > a > img").attr("src", "https://annie-mac.com/uploads/sites/10713/public/PurchasePros.png");
+	// 	$("#navigation-logo > a > img").css("margin-top", "0px");
+	// }
+	// // Team: Team Shealey
+	// if (window.location.href.indexOf("katy.annie-mac.com") > -1) {
+	// 	$("#navigation-logo > a > img").attr("src", "https://annie-mac.com/uploads/sites/10713/public/Shealey.png");
+	// 	$("#navigation-logo > a > img").css("margin-top", "0px");
+	// }
+	// // Team: The Home Team
+	// if (window.location.href.indexOf("stevevetter.annie-mac.com") > -1) {
+	// 	$("#navigation-logo > a > img").attr("src", "https://annie-mac.com/uploads/sites/10713/public/TheHomeTeam.png");
+	// 	$("#navigation-logo > a > img").css("margin-top", "7px");
+	// }
+	// // Team: Trye
+	// if (window.location.href.indexOf("towson.annie-mac.com") > -1) {
+	// 	$("#navigation-logo > a > img").attr("src", "https://annie-mac.com/uploads/sites/10713/public/Trye.png");
+	// 	$("#navigation-logo > a > img").css("margin-top", "0px");
+	// }
+	// // Team: kevinhulsey
+	// if (window.location.href.indexOf("kevinhulsey.annie-mac.com") > -1) {
+	// 	$("#navigation-logo > a > img").attr("src", "https://annie-mac.com/uploads/sites/10713/public/hulsey.png");
+	// 	$("#navigation-logo > a > img").css("margin-top", "3px");
+	// }
