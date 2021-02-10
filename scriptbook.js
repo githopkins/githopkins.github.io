83d7596701bed9a114a453950b9508f289d576c9
Nick Hopkins <nick@macbook-pro.local>
2021-02-10 05:55:13 +0000

     

--- scriptbook.js
+++ scriptbook.js
@@ -1,6 +1,7 @@
 // Variables to drop HTML below:
 var loanoriginatorname = document.querySelector("#originator-core-details-text > h2").innerText;
 var loanoriginatornmls  = document.querySelector("#originator-core-details-text > h4 > span").innerText;
+var firstname = loanoriginatorname.split(" ");
 var bookingLinkLoanOriginatorDrop = '<a href="/page/bookings"><button style="background: #78c254">Book</button></a>';
 var prequalLinkLoanOriginatorDrop = '<a href="/loan/graphic-prequal"><button>Prequal</button></a>';
 var covid = '<div id="covid-warning"><h4>To those affected by COVID-19</h4><div id="covid-warning-container"><p>We are available to review your options with you and explain how to apply for relief. Borrowers interested in contacting AnnieMac to discuss payment assistance during the COVID19 pandemic can reach us using the options below.</p><a href="tel:877-204-1868">Call</a> <a href="mailto:CustomerService@annie-mac.com">Email</a></div></div>';
@@ -29,7 +30,7 @@ $(document).ready(function() {
 if ($('body').hasClass('site-type-loan_officer')) {
 	$(dropbanner).insertBefore( $( "#annie_mac-loan-officer" ) );
 	$(fullwidthsidebarlinks).append("#loan-originator-blurb-container");
-	$("#we-provide-more-master > h3").text(loanoriginatorname + ' Provides...');
+	$("#we-provide-more-master > h3").text(firstname[0] + ' Provides...');
 	// $(dropbanner).insertAfter('#site-navigation');
 	$( ".manager-biography h2" ).prepend("About ");
 	// $("#apply-navigation").css("display", "flex");
