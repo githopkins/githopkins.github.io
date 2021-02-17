5a16e7381a8d54c670646e12d86c09393ce51d7a
Nick Hopkins <nick@macbook-pro.local>
2021-02-17 21:50:49 +0000

     

--- scriptbook.js
+++ scriptbook.js
@@ -1,7 +1,4 @@
 // Variables to drop HTML below:
-var loanoriginatorname = document.querySelector("#originator-core-details-text > h2").innerText;
-var loanoriginatornmls  = document.querySelector("#originator-core-details-text > h4 > span").innerText;
-var firstname = loanoriginatorname.split(" ");
 var bookingLinkLoanOriginatorDrop = '<a href="/page/bookings"><button style="background: #78c254">Book</button></a>';
 var prequalLinkLoanOriginatorDrop = '<a href="/loan/graphic-prequal"><button>Prequal</button></a>';
 var covid = '<div id="covid-warning"><h4>To those affected by COVID-19</h4><div id="covid-warning-container"><p>We are available to review your options with you and explain how to apply for relief. Borrowers interested in contacting AnnieMac to discuss payment assistance during the COVID19 pandemic can reach us using the options below.</p><a href="tel:877-204-1868">Call</a> <a href="mailto:CustomerService@annie-mac.com">Email</a></div></div>';
@@ -12,13 +9,21 @@ var fullwidthsidebarlinks = '<div id="links-sidebar-full-width"><h4>Important Li
 
 $( document ).ready(function() {
 	if ($('body').hasClass('site-type-loan_officer')) {	
+		var loanoriginatorname = document.querySelector("#originator-core-details-text > h2").innerText;
+		var loanoriginatornmls  = document.querySelector("#originator-core-details-text > h4 > span").innerText;
+		var firstname = loanoriginatorname.split(" ");
 		$(dropbanner).insertBefore( $( "#annie_mac-loan-officer" ) );
 		$(covid).insertBefore( $( "#biography" ) );
 		$(fullwidthsidebarlinks).appendTo( $( "#loan-originator-backdrop" ) );
 		$("#we-provide-more-master > h3").text(firstname[0] + ' Provides...');
 		$("#biography > div.manager-biography > h2").text('About ' + firstname[0]);
 	};
+	if ($('body').hasClass('site-type-branch')) {
+		$(covid).insertBefore( $( "#home-provide" ) );
+	}
 });
+
+	
 	// Domains
 	// if (
 	// 	(window.location.href.indexOf("jamiehannigan.annie-mac.com") > -1) || 		
