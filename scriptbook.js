d1d0b252eeebf35cdde8a6e39562af698489fc90
Nick Hopkins <nick@macbook-pro.local>
2021-02-26 18:26:41 +0000

     

--- scriptbook.js
+++ scriptbook.js
@@ -3,8 +3,8 @@ $( document ).ready(function() {
 		$('#beginner-guide-to-mortgages').hide();
 		localStorage.setItem('podcastInformation', false);
 	});
-	if(podcastInformation){
-		$('#beginner-guide-to-mortgages').show();
+	if(podcastInformation == false){
+		$('#beginner-guide-to-mortgages').hide();
 	};
 	if ($('body').hasClass('site-type-loan_officer')) {	
 		var covid = '<div id="covid-warning"><h4>To those affected by COVID-19</h4><div id="covid-warning-container"><p>We are available to review your options with you and explain how to apply for relief. Borrowers interested in contacting AnnieMac to discuss payment assistance during the COVID19 pandemic can reach us using the options below.</p><a href="tel:877-204-1868">Call</a> <a href="mailto:CustomerService@annie-mac.com">Email</a></div></div>';
