b3612df08b0a08234ff600f16aa7c69f8d9fae3e
Nick Hopkins <nick@macbook-pro.local>
2021-03-24 02:52:54 +0000

     

--- scriptbook.js
+++ scriptbook.js
@@ -1,4 +1,12 @@
 $( document ).ready(function() {
+	$(function() {
+	const locationListings = $(".location-listing");
+		locationListing.forEach((LocationListing) => {
+		const domain = locationListing.find("h4 a").attr('href');
+		const subdomain = domain.replace('.annie-mac.com', '').replace('https://', '');
+		locationListing.id = subdomain;
+	})
+	});
 	$('#hide-popover').click(function() {
 		$('#beginner-guide-to-mortgages').hide();
 		localStorage.setItem('podcastpopuphidden', 'true');
