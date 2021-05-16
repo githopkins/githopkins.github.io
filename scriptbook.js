4f678f03ecb8f4d8bcd0897218ecd455a56071df
Nick Hopkins <nick@macbook-pro.local>
2021-05-16 04:13:03 +0000

     

--- scriptbook.js
+++ scriptbook.js
@@ -369,8 +369,7 @@ $(function() {
 			var valid = /^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$/.test(this.value),
 				val = this.value;
 			if (valid) {
-				$('#submit-overlayment')
-					.hide();
+				$('#submit-overlayment').hide();
 			}
 		});
 		$(":input").on("keyup change", function(e) {
