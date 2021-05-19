429855b9211d63737d4808e83f415378d7665ace
Nick Hopkins <nick@macbook-pro.local>
2021-05-19 20:30:20 +0000

     

--- scriptbook.js
+++ scriptbook.js
@@ -94,6 +94,7 @@ $(function() {
 		}
 	};
 	if (window.location.href.indexOf("scottsynovic") != -1) {
+		$("#list_12148 > a:nth-child(1)").attr("href", "tel:303-668-3350");
 		$("#biography > div.manager-biography > h2").text("Scott Synovic is a Lifelong, Dedicated, Mortgage Loan Originator.");
 	}
 	if (window.location.href.indexOf("kevinhulsey") != -1) {
