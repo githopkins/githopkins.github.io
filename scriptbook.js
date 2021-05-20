33a2b68ec3ff6a3439f319ca707400c8a299149d
Nick Hopkins <nick@macbook-pro.local>
2021-05-20 14:38:54 +0000

     

--- scriptbook.js
+++ scriptbook.js
@@ -37,7 +37,7 @@ $(function() {
 	if ($('body').hasClass('site-type-loan_officer')) {
 		if (document.cookie.indexOf("visitedloanoriginator=") < 0) {
 		  const value = window.location.host.split('.')[0];
-		  document.cookie = "visitedloanoriginator=" + value + "; path=/; domain=.annie-mac.com";
+		  document.cookie = "visitedloanoriginator=" + value + "; path=/; max-age=2592000; domain=.annie-mac.com";
 		  var loanofficerlastvisited = "https://" + readCookie('visitedloanoriginator') + ".annie-mac.com";
 		  console.log("A cookie has been created for " + loanofficerlastvisited);
 		}
