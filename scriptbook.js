680d9e7904b2f1ac92567ea3f9aabdb8f90ff296
Nick Hopkins <nick@macbook-pro.local>
2021-05-20 01:01:34 +0000

     

--- scriptbook.js
+++ scriptbook.js
@@ -36,7 +36,7 @@ $(function() {
 	if ($('body').hasClass('site-type-branch')) {	
 		$("#team-display > h3:nth-child(1)").text("Branch Management");
 	};
-	localStorage.getItem('cookiecreated');
+	localStorage.setItem('cookiecreated', 'false');
 	if ($('body').hasClass('site-type-loan_officer')) {	
 		if ((localStorage.cookiecreated) == "false") {
 			cookiename = window.location.host.split('.')[1] ? window.location.host.split('.')[0] : false;
