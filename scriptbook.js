63cc553d11c5dc07cb586f1dfc9c7b2a0710c769
Nick Hopkins <nick@macbook-pro.local>
2021-05-20 02:00:01 +0000

     

--- scriptbook.js
+++ scriptbook.js
@@ -38,7 +38,8 @@ $(function() {
 		if (document.cookie.indexOf("visitedloanoriginator=") < 0) {
 		  const value = window.location.host.split('.')[1] ? window.location.host.split('.')[0] : false;
 		  document.cookie = "visitedloanoriginator=" + value + "; path=/; domain=.annie-mac.com";
-		  console.log("a cookie has been created");
+		  var loanofficerlastvisited = "https://" + readCookie('visitedloanoriginator') + ".annie-mac.com";
+		  console.log("A cookie has been created for" + loanofficerlastvisited);
 		}
 		var covid = '<div id="covid-warning"><h4>To those affected by COVID-19 <span id="dismisscovid">Dismiss</span></h4><div id="covid-warning-container"><p>We are available to review your options with you and explain how to apply for relief. Borrowers interested in contacting AnnieMac to discuss payment assistance during the COVID19 pandemic can reach us using the options below.</p><a href="tel:877-204-1868">Call</a> <a href="mailto:CustomerService@annie-mac.com">Email</a></div></div>';
 		$(covid).insertBefore( $( "#biography" ) );
