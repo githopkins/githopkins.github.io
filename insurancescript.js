6e03ffbf9cf1ab80b64a2b0c5347bce67748bc12
Nick Hopkins <nick@macbook-pro.local>
2021-06-22 00:03:55 +0000

     

--- insurancescript.js
+++ insurancescript.js
@@ -1,19 +1,19 @@
 $(function() {
-	$("#sales-team").hide('slow'); 
-	$("#client-team").hide('slow'); 
+	$("#sales-team").fadeOut('slow');
+	$("#client-team").fadeOut('slow');
 	$("#sales-toggle").click(function() {
-		$("#leadership-team").hide('slow'); 
-		$("#sales-team").show('slow'); 
-		$("#client-team").hide('slow');
+		$("#leadership-team").fadeOut('slow'); 
+		$("#sales-team").fadeIn('slow'); 
+		$("#client-team").fadeOut('slow');
 	};
 	$("#leadership-toggle").click(function() {
-		$("#leadership-team").show('slow'); 
-		$("#sales-team").hide('slow'); 
-		$("#client-team").hide('slow');
+		$("#leadership-team").fadeIn('slow'); 
+		$("#sales-team").fadeOut('slow'); 
+		$("#client-team").fadeOut('slow');
 	};
 	$("#client-toggle").click(function() {
-		$("#leadership-team").hide('slow'); 
-		$("#sales-team").hide('slow'); 
-		$("#client-team").show('slow');
+		$("#leadership-team").fadeOut('slow'); 
+		$("#sales-team").fadeOut('slow'); 
+		$("#client-team").fadeIn('slow');
 	};
 });
