d787bd61ca02b0cb61c495a25fdafca0e230ef64
Nick Hopkins <nick@macbook-pro.local>
2021-06-27 03:06:58 +0000

     

--- annie-mac-react.js
+++ annie-mac-react.js
@@ -1,28 +1,8 @@
-'use strict';
-
-const e = React.createElement;
-const element = '<h1>Hello, world</h1>';
-
-// class LikeButton extends React.Component {
-//   constructor(props) {
-// 	super(props);
-// 	this.state = { liked: false };
-//   }
-// 
-//   render() {
-// 	<h1>Hello, world!</h1>
-// 	if (this.state.liked) {
-// 	  return 'You liked this.';
-// 	}
-// 
-// 	return e(	
-// 	  'button',
-// 	  { onClick: () => this.setState({ liked: true }) },
-// 	  'Like'
-// 	);
-//   }
-// }
-
-// const domContainer = document.querySelector('#react-test');
-// ReactDOM.render(e(LikeButton), domContainer);
-ReactDOM.render(element, document.getElementById('#react-test'));
+const element = (
+	<div>
+	  <h1>Hello, world!</h1>
+	  <h2>It is {new Date().toLocaleTimeString()}.</h2>
+	</div>
+  );
+  ReactDOM.render(element, document.getElementById('root'));
+}
