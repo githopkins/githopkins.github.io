8d10ec4fa527734827c7a2bed3afcca99a357d1c
Nick Hopkins <nick@macbook-pro.local>
2021-06-27 04:42:35 +0000

    √

--- annie-mac-react.js
+++ annie-mac-react.js
@@ -2,12 +2,20 @@
 const domContainer = document.querySelector('#react-test');
 const e = React.createElement;
 
-if (window.location.href.indexOf("react-test") != -1) {
-	import FirstTimeHomeBuyerGuide from 'components'
-	ReactDOM.render(e(FirstTimeHomeBuyerGuide), domContainer);
-};
+class Car extends React.Component {
+	  render() {
+		return <h2>Hi, I am a Car!</h2>;
+	  }
+	}
+	
+ReactDOM.render(<Car />, document.getElementById('react-test'));
 
-if (window.location.href.indexOf("testing-react") != -1) {
-	import testingreact from 'components'
-	ReactDOM.render(e(testingreact), domContainer);
-};
+// if (window.location.href.indexOf("react-test") != -1) {
+// 	import FirstTimeHomeBuyerGuide from 'components.js'
+// 	ReactDOM.render(e(FirstTimeHomeBuyerGuide), domContainer);
+// };
+// 
+// if (window.location.href.indexOf("testing-react") != -1) {
+// 	import testingreact from 'components'
+// 	ReactDOM.render(e(testingreact), domContainer);
+// };
