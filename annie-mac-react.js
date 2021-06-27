2c578a658255081bbda64b8e03229d4956cadfe6
Nick Hopkins <nick@macbook-pro.local>
2021-06-27 03:22:58 +0000

     

--- annie-mac-react.js
+++ annie-mac-react.js
@@ -1,41 +1,17 @@
 'use strict';
-
+const domContainer = document.querySelector('#react-test');
 const e = React.createElement;
-
-class Welcome extends React.Component {
+// Declaration of Componenets
+class FirstTimeHomeBuyerGuide extends React.Component {
 	render() {
 		return (
-		  <div className="shopping-list">
-			<h1>Shopping List for {this.props.name}</h1>
-			<ul>
-			  <li>Instagram</li>
-			  <li>WhatsApp</li>
-			  <li>Oculus</li>
-			</ul>
-		  </div>
+			<div id="annie-mac-2021-content-container">
+				<div class="annie-mac-2021-redesign-section" id="product-information-section">
+					<h2>First Time Home Buyer Guide</h2>
+				</div>
+			</div>
 		);
 	  }
 }
 
-// class LikeButton extends React.Component {
-//   constructor(props) {
-// 	super(props);
-// 	this.state = { liked: false };
-//   }
-// 
-//   render() {
-// 	if (this.state.liked) {
-// 	  return 'You liked this.';
-// 	}
-// 
-// 	return e(
-// 	  'button',
-// 	  { onClick: () => this.setState({ liked: true }) },
-// 	  'Like'
-// 	);
-//   }
-// }
-
-const domContainer = document.querySelector('#react-test');
-// ReactDOM.render(e(LikeButton), domContainer);
-ReactDOM.render(e(Welcome), domContainer);
+ReactDOM.render(e(FirstTimeHomeBuyerGuide), domContainer);
