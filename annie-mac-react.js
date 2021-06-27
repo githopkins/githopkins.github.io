d75a1887477884ba0506604d7def4c41328f6fd2
Nick Hopkins <nick@macbook-pro.local>
2021-06-27 04:40:48 +0000

     

--- annie-mac-react.js
+++ annie-mac-react.js
@@ -3,9 +3,11 @@ const domContainer = document.querySelector('#react-test');
 const e = React.createElement;
 
 if (window.location.href.indexOf("react-test") != -1) {
+	import FirstTimeHomeBuyerGuide from 'components'
 	ReactDOM.render(e(FirstTimeHomeBuyerGuide), domContainer);
 };
 
 if (window.location.href.indexOf("testing-react") != -1) {
+	import testingreact from 'components'
 	ReactDOM.render(e(testingreact), domContainer);
 };
