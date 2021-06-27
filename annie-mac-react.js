19b90de2706ac76dd3ab33170525190156bf123c
Nick Hopkins <nick@macbook-pro.local>
2021-06-27 04:19:21 +0000

     

--- annie-mac-react.js
+++ annie-mac-react.js
@@ -3,31 +3,16 @@ const domContainer = document.querySelector('#react-test');
 const e = React.createElement;
 
 // Declaration of Componenets
-class FirstTimeHomeBuyerGuideHeaderContent extends React.Component {
+class FirstTimeHomeBuyerGuide extends React.Component {
 	render() {
 		return (
 			<div id="annie-mac-2021-content-container">
 				<div class="annie-mac-2021-redesign-section" id="product-information-section">
 					<h2>First Time Home Buyer Guide</h2>
-		);
-	  }
-}
-
-class FirstTimeHomeBuyerGuideParagraphContent extends React.Component {
-	render() {
-		return (
-					<p>This should show.</p>
 				</div>
 			</div>
 		);
 	  }
-}
-
-class FirstTimeHomeBuyerGuide extends React.Component {
-	render() {
-		return FirstTimeHomeBuyerGuideHeaderContent
-		return FirstTimeHomeBuyerGuideParagraphContent
-	}
 }
 
 ReactDOM.render(e(FirstTimeHomeBuyerGuide), domContainer);
