48ff1a23ab93be5850aef446d69fbde8da8a066f
Nick Hopkins <nick@macbook-pro.local>
2021-06-27 04:00:21 +0000

     

--- annie-mac-react.js
+++ annie-mac-react.js
@@ -1,6 +1,7 @@
 'use strict';
 const domContainer = document.querySelector('#react-test');
 const e = React.createElement;
+
 // Declaration of Componenets
 class FirstTimeHomeBuyerGuide extends React.Component {
 	render() {
@@ -14,4 +15,16 @@ class FirstTimeHomeBuyerGuide extends React.Component {
 	  }
 }
 
+class FirstTimeHomeBuyerGuide2 extends React.Component {
+	render() {
+		return (
+			<div id="annie-mac-2021-content-container">
+				<div class="annie-mac-2021-redesign-section" id="product-information-section">
+					<h2>First Time Home Buyer Guide 2</h2>
+				</div>
+			</div>
+		);
+	  }
+}
 ReactDOM.render(e(FirstTimeHomeBuyerGuide), domContainer);
+ReactDOM.render(e(FirstTimeHomeBuyerGuide2), domContainer);
