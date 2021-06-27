6287db2a39791386f97c46db29f1dfeddcc77f24
Nick Hopkins <nick@macbook-pro.local>
2021-06-27 04:23:04 +0000

     

--- annie-mac-react.js
+++ annie-mac-react.js
@@ -15,6 +15,22 @@ class FirstTimeHomeBuyerGuide extends React.Component {
 	  }
 };
 
+class testingreact extends React.Component {
+	render() {
+		return (
+			<div id="annie-mac-2021-content-container">
+				<div class="annie-mac-2021-redesign-section" id="product-information-section">
+					<h2>Just Testing React</h2>
+				</div>
+			</div>
+		);
+	  }
+};
+
 if (window.location.href.indexOf("react-test") != -1) {
 ReactDOM.render(e(FirstTimeHomeBuyerGuide), domContainer);
 };
+
+if (window.location.href.indexOf("testing-react") != -1) {
+	ReactDOM.render(e(testingreact), domContainer);
+	};
