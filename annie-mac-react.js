98f317835143fa171250659bea4d27ab7a6b7edb
Nick Hopkins <nick@macbook-pro.local>
2021-06-27 03:17:08 +0000

     

--- annie-mac-react.js
+++ annie-mac-react.js
@@ -4,8 +4,17 @@ const e = React.createElement;
 
 class Welcome extends React.Component {
 	render() {
-		return <h1>Hello, {this.props.name}</h1>;
-	}
+		return (
+		  <div className="shopping-list">
+			<h1>Shopping List for {this.props.name}</h1>
+			<ul>
+			  <li>Instagram</li>
+			  <li>WhatsApp</li>
+			  <li>Oculus</li>
+			</ul>
+		  </div>
+		);
+	  }
 }
 
 // class LikeButton extends React.Component {
