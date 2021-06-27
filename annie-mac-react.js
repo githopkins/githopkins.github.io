e15878d9f9782606eb5182467341330a95bf668b
Nick Hopkins <nick@macbook-pro.local>
2021-06-27 04:37:30 +0000

     

--- annie-mac-react.js
+++ annie-mac-react.js
@@ -2,31 +2,6 @@
 const domContainer = document.querySelector('#react-test');
 const e = React.createElement;
 
-// Declaration of Componenets
-class FirstTimeHomeBuyerGuide extends React.Component {
-	render() {
-		return (
-			<div id="annie-mac-2021-content-container">
-				<div class="annie-mac-2021-redesign-section" id="product-information-section">
-					<h2>First Time Home Buyer Guide</h2>
-				</div>
-			</div>
-		);
-	}
-};
-
-class testingreact extends React.Component {
-	render() {
-		return (
-			<div id="annie-mac-2021-content-container">
-				<div class="annie-mac-2021-redesign-section" id="product-information-section">
-					<h2>Just Testing React</h2>
-				</div>
-			</div>
-		);
-	}
-};
-
 if (window.location.href.indexOf("react-test") != -1) {
 	ReactDOM.render(e(FirstTimeHomeBuyerGuide), domContainer);
 };
---
+++ components.js
@@ -0,0 +1,24 @@
+// Declaration of Componenets
+class FirstTimeHomeBuyerGuide extends React.Component {
+	render() {
+		return (
+			<div id="annie-mac-2021-content-container">
+				<div class="annie-mac-2021-redesign-section" id="product-information-section">
+					<h2>First Time Home Buyer Guide</h2>
+				</div>
+			</div>
+		);
+	}
+};
+
+class testingreact extends React.Component {
+	render() {
+		return (
+			<div id="annie-mac-2021-content-container">
+				<div class="annie-mac-2021-redesign-section" id="product-information-section">
+					<h2>Just Testing React</h2>
+				</div>
+			</div>
+		);
+	}
+};
