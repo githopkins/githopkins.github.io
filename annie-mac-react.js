2095f3165a7c751a1b662dcb9aec5990f5eb23f0
Nick Hopkins <nick@macbook-pro.local>
2021-06-27 16:21:17 +0000

     

--- annie-mac-react.js
+++ annie-mac-react.js
@@ -104,7 +104,6 @@ if (window.location.href.indexOf("testing-react") != -1) {
 	ReactDOM.render(e(UnderstandingCreditScores), domContainer);
 	$(function() {
 		$('#credit-image').prepend('<img src="https://annie-mac.com/uploads/sites/10713/public/credit_score.jpeg" />');
-	})
-	
+	})	
 };
 
