e3774324254e154846293bb5ec11d13145cebc08
Nick Hopkins <nick@macbook-pro.local>
2021-06-27 03:16:35 +0000

     

--- annie-mac-react.js
+++ annie-mac-react.js
@@ -4,30 +4,29 @@ const e = React.createElement;
 
 class Welcome extends React.Component {
 	render() {
-		return 'Hi...';
-		return '<h1>Hi!!.</h1>';
+		return <h1>Hello, {this.props.name}</h1>;
 	}
 }
 
-class LikeButton extends React.Component {
-  constructor(props) {
-	super(props);
-	this.state = { liked: false };
-  }
-
-  render() {
-	if (this.state.liked) {
-	  return 'You liked this.';
-	}
-
-	return e(
-	  'button',
-	  { onClick: () => this.setState({ liked: true }) },
-	  'Like'
-	);
-  }
-}
+// class LikeButton extends React.Component {
+//   constructor(props) {
+// 	super(props);
+// 	this.state = { liked: false };
+//   }
+// 
+//   render() {
+// 	if (this.state.liked) {
+// 	  return 'You liked this.';
+// 	}
+// 
+// 	return e(
+// 	  'button',
+// 	  { onClick: () => this.setState({ liked: true }) },
+// 	  'Like'
+// 	);
+//   }
+// }
 
 const domContainer = document.querySelector('#react-test');
-ReactDOM.render(e(LikeButton), domContainer);
+// ReactDOM.render(e(LikeButton), domContainer);
 ReactDOM.render(e(Welcome), domContainer);
