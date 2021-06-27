d1f1937313947aa73fa48f011a7a57c72cb0723b
Nick Hopkins <nick@macbook-pro.local>
2021-06-27 03:10:38 +0000

     

--- annie-mac-react.js
+++ annie-mac-react.js
@@ -1,14 +1,24 @@
-
-
 'use strict';
 
 const e = React.createElement;
+
 class LikeButton extends React.Component {
-return e(
+  constructor(props) {
+	super(props);
+	this.state = { liked: false };
+  }
+
+  render() {
+	if (this.state.liked) {
+	  return 'You liked this.';
+	}
+
+	return e(
 	  'button',
 	  { onClick: () => this.setState({ liked: true }) },
 	  'Like'
 	);
+  }
 }
 
 const domContainer = document.querySelector('#react-test');
