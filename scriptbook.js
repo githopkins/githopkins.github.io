ac592af839ed5aa2f54f66fe6e9deb212f778f8e
Nick Hopkins <nick@macbook-pro.local>
2021-06-30 01:09:24 +0000

     

--- scriptbook.js
+++ scriptbook.js
@@ -749,7 +749,7 @@ $(function() {
 		$("#product-type-conventional").removeAttr('checked');
 		$("#product-type-fha").removeAttr('checked');
 	});
-	// For new pages, remove old stylesheets and add the new 2021 stylesheet.
+	// For new pages and the blog, remove old stylesheets and add the new 2021 stylesheet.
 	if ((window.location.href.indexOf("credit-score-basics") != -1) || (window.location.href.indexOf("blog") != -1) || (window.location.href.indexOf("prequal-vs-preapproval") != -1)) {
 		$('head').append('<link rel="stylesheet" href="https://githopkins.github.io/anniemac2021corporate.css" type="text/css" />');
 		$("head > link:nth-child(37)").remove();
